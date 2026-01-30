212ae4aaac3087593f3b65a336b172a0de22342e
nuhazzz <nurizqis@gmail.com>
2026-01-30 11:17:40 +0700

    Add files via upload

---
+++ pengaduan_sekolah.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 30 Jan 2026 pada 05.05
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pengaduan_sekolah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+-- Kesalahan membaca struktur untuk tabel pengaduan_sekolah.admin: #1932 - Table 'pengaduan_sekolah.admin' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel pengaduan_sekolah.admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pengaduan_sekolah`.`admin`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `aspirasi`
+--
+-- Kesalahan membaca struktur untuk tabel pengaduan_sekolah.aspirasi: #1932 - Table 'pengaduan_sekolah.aspirasi' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel pengaduan_sekolah.aspirasi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pengaduan_sekolah`.`aspirasi`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `input_aspirasi`
+--
+-- Kesalahan membaca struktur untuk tabel pengaduan_sekolah.input_aspirasi: #1932 - Table 'pengaduan_sekolah.input_aspirasi' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel pengaduan_sekolah.input_aspirasi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pengaduan_sekolah`.`input_aspirasi`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+-- Kesalahan membaca struktur untuk tabel pengaduan_sekolah.kategori: #1932 - Table 'pengaduan_sekolah.kategori' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel pengaduan_sekolah.kategori: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pengaduan_sekolah`.`kategori`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `siswa`
+--
+-- Kesalahan membaca struktur untuk tabel pengaduan_sekolah.siswa: #1932 - Table 'pengaduan_sekolah.siswa' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel pengaduan_sekolah.siswa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pengaduan_sekolah`.`siswa`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
